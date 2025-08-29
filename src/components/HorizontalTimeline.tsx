43d0044ff1974b59c3e6a882a0f330ba735bcc18
nprasadgt <nprasadgt@gmail.com>
2025-08-29 10:05:41 +0530

    Update HorizontalTimeline.tsx

--- src/components/HorizontalTimeline.tsx
+++ src/components/HorizontalTimeline.tsx
@@ -1,5 +1,5 @@
-import { Button } from '@/components/ui/button';
-import { Card, CardContent } from '@/components/ui/card';
+import { Button } from '../components/ui/button';
+import { Card, CardContent } from '../components/ui/card';
 import useEmblaCarousel from 'embla-carousel-react';
 import { motion } from 'framer-motion';
 import {
