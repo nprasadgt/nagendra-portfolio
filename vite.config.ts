033b7b1f3794c1af513b94c3f52b0d67cc487b46
nprasadgt <nprasadgt@gmail.com>
2025-08-29 09:42:56 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/nagendra-portfolio/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
